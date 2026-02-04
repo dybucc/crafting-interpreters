c2506b253645435b013a88f0054e7e682474ff3b
dybucc <149513579+dybucc@users.noreply.github.com>
2026-02-04 19:00:48 +0100

    chore: keep working on the list challenge

--- challenges/src/lib.rs
+++ challenges/src/lib.rs
@@ -1,10 +1,15 @@
 #![expect(dead_code, reason = "WIP.")]
 
-use std::{marker::PhantomData, mem};
+use std::{
+    cell::{Ref, RefCell},
+    marker::PhantomData,
+    ops::Deref,
+    rc::Rc,
+};
 
 struct DoublyLinkedList<T> {
-    start: Option<Box<Node<T>>>,
-    end: Option<Box<Node<T>>>,
+    start: Option<Rc<RefCell<Node<T>>>>,
+    end: Option<Rc<RefCell<Node<T>>>>,
     _marker: PhantomData<T>,
 }
 
@@ -18,23 +23,38 @@ impl<T> DoublyLinkedList<T> {
     }
 
     fn insert_at(&mut self, other: T, pos: InsertionPos) -> Option<()> {
+        let new = Node {
+            left: None,
+            right: None,
+            inner: other,
+        };
+
         match pos {
             InsertionPos::Start => {
-                let new = Node {
-                    left: None,
-                    right: None,
-                    inner: other,
-                };
-                let Some(start) = &mut self.start else {
-                    self.start = Some(Box::new(new));
+                let Some(start) = &self.start else {
+                    self.start = Some(Rc::new(RefCell::new(new)));
+                    self.end = Some(Rc::clone(self.start.as_ref().unwrap()));
+
                     return Some(());
                 };
+                let mut old = start.replace(new);
 
-                let mut new = Box::new(new);
-                mem::swap(&mut new, start);
+                old.left = Some(Rc::clone(start));
+                start.borrow_mut().right = Some(Rc::new(RefCell::new(old)));
             }
-            InsertionPos::End => todo!(),
-            InsertionPos::Index(idx) => todo!(),
+            InsertionPos::End => {
+                let Some(end) = &self.end else {
+                    self.start = Some(Rc::new(RefCell::new(new)));
+                    self.end = Some(Rc::clone(self.start.as_ref().unwrap()));
+
+                    return Some(());
+                };
+                let mut old = end.replace(new);
+
+                old.right = Some(Rc::clone(end));
+                end.borrow_mut().left = Some(Rc::new(RefCell::new(old)));
+            }
+            InsertionPos::Index(idx) => (),
         }
 
         Some(())
@@ -50,8 +70,8 @@ impl<T> DoublyLinkedList<T> {
 
     fn iter(&self) -> Iter<'_, T> {
         Iter {
-            first: self.start.as_deref(),
-            last: self.end.as_deref(),
+            first: self.start.as_ref().map(|elem| elem.as_ref().borrow()),
+            last: self.end.as_ref().map(|elem| elem.as_ref().borrow()),
             current: None,
             _marker: PhantomData,
         }
@@ -85,23 +105,35 @@ enum InsertionPos {
 }
 
 struct Node<T> {
-    left: Option<Box<Node<T>>>,
-    right: Option<Box<Node<T>>>,
+    left: Option<Rc<RefCell<Node<T>>>>,
+    right: Option<Rc<RefCell<Node<T>>>>,
     inner: T,
 }
 
 struct Iter<'a, T> {
-    first: Option<&'a Node<T>>,
-    last: Option<&'a Node<T>>,
-    current: Option<&'a Node<T>>,
+    first: Option<Ref<'a, Node<T>>>,
+    last: Option<Ref<'a, Node<T>>>,
+    current: Option<Rc<Node<T>>>,
     _marker: PhantomData<&'a T>,
 }
 
 impl<'a, T> Iterator for Iter<'a, T> {
-    type Item = &'a T;
+    type Item = Ref<'a, Node<T>>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+        match self.current {
+            None => {
+                let Some(first) = &self.first else {
+                    return None;
+                };
+                let Some(last) = &self.last else {
+                    return None;
+                };
+            }
+            Some(_) => todo!(),
+        }
+
+        self.first.as_ref()
     }
 }
 
@@ -109,19 +141,19 @@ impl<'a, T> Iterator for Iter<'a, T> {
 mod tests {
     use super::*;
 
-    #[test]
-    fn it_works() {
-        macro_rules! insertion_test {
-            ($src:expr, $insertion:expr, $list:expr, $pos:expr) => {{
-                $src.insert_at($insertion, $pos);
-                assert!(
-                    $src.iter()
-                        .map(|elem| elem.as_bytes())
-                        .eq($list.iter().map(|elem| elem.as_bytes()))
-                );
-            }};
-        }
+    macro_rules! insertion_test {
+        ($src:expr, $insertion:expr, $list:expr, $pos:expr) => {{
+            $src.insert_at($insertion, $pos);
+            assert!(
+                $src.iter()
+                    .map(|elem| elem.as_bytes())
+                    .eq($list.iter().map(|elem| elem.as_bytes()))
+            );
+        }};
+    }
 
+    #[test]
+    fn insertion_at_start() {
         let mut list = DoublyLinkedList::from(["Something", "else"]);
 
         insertion_test!(
@@ -130,21 +162,50 @@ mod tests {
             ["Something else", "Something", "else"],
             InsertionPos::Start
         );
+    }
+
+    #[test]
+    fn insertion_at_end() {
+        let mut list = DoublyLinkedList::from(["Something", "else"]);
+
         insertion_test!(
             list,
             "Nothing",
-            ["Something else", "Something", "else", "Nothing",],
+            ["Something", "else", "Nothing",],
             InsertionPos::End
         );
+    }
+
+    #[test]
+    fn insertion_at_idx_correct() {
+        let mut list = DoublyLinkedList::from(["Something", "else"]);
+
         insertion_test!(
             list,
             "NUMA",
-            ["Something else", "Something", "NUMA", "else", "Nothing",],
-            InsertionPos::Index(2)
+            ["Something", "NUMA", "else"],
+            InsertionPos::Index(1)
         );
+    }
+
+    #[test]
+    fn insertion_at_idx_incorrect() {
+        let mut list = DoublyLinkedList::from(["Something", "else"]);
+
+        todo!();
+    }
+
+    #[test]
+    fn search() {
+        let list = DoublyLinkedList::from(["Something", "else"]);
 
         assert_eq!(list.find(&"else"), Some(1));
         assert_eq!(list.find(&"nothing"), None);
+    }
+
+    #[test]
+    fn deletion() {
+        let mut list = DoublyLinkedList::from(["Something", "else"]);
 
         assert_eq!(list.delete(&"Something"), "Something");
     }
