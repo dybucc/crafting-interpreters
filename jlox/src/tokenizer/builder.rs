7bc3dfdffbe01759b4dfcba386df42204212c778
dybucc <149513579+dybucc@users.noreply.github.com>
2026-05-18 14:08:30 +0200

    chore: wip

--- jlox/src/tokenizer/builder.rs
+++ jlox/src/tokenizer/builder.rs
@@ -1,4 +1,4 @@
-use crate::tokenizer::{Location, Token, TokenType};
+use crate::tokenizer::{Lexeme, Lit, Location, Token, TokenType};
 
 // TODO: finish the below interface.
 
@@ -9,39 +9,53 @@ pub(crate) struct TokenBuilder {
 
 impl TokenBuilder {
     pub(crate) fn finalize(&mut self) -> Token {
-        let Self { repr } = self;
+        let TokenBuilder { repr } = self;
 
         repr.finalize()
     }
 
+    pub(crate) fn with_lexeme(&mut self, lex: impl AsRef<[u8]>) -> &mut Self {
+        let TokenBuilder { repr } = self;
+
+        repr.add_lexeme(lex);
+
+        self
+    }
+
     pub(crate) fn single(&mut self, bytes: &[u8], loc: Location) -> &mut Self {
-        let Self { repr } = self;
+        let TokenBuilder { repr } = self;
+
+        debug_assert_eq!(
+            bytes.len(),
+            1,
+            "bytes in single token constructor should be made up of a single raw byte"
+        );
 
         match repr.kind() {
             TokenKind::Empty => *repr = TokenRepr::single(bytes, loc),
-            TokenKind::Configuration => repr.recycle(bytes, loc),
+            TokenKind::Configuration => repr.recycle_single(*bytes.first().unwrap(), loc),
         }
 
         self
     }
 
     pub(crate) fn compound(&mut self, bytes: &[u8], loc: Location) -> &mut Self {
-        let Self { repr } = self;
+        let TokenBuilder { repr } = self;
 
         match repr.kind() {
             TokenKind::Empty => *repr = TokenRepr::compound(bytes, loc),
-            TokenKind::Configuration => repr.recycle(bytes, loc),
+            TokenKind::Configuration => repr.recycle_compound(bytes, loc),
         }
 
         self
     }
 
     pub(crate) fn multiple(&mut self, bytes: &[u8], hint: TokenType, loc: Location) -> &mut Self {
-        let Self { repr } = self;
+        let TokenBuilder { repr } = self;
 
         match repr.kind() {
             TokenKind::Empty => *repr = TokenRepr::multiple(bytes, hint, loc),
-            TokenKind::Configuration => repr.recycle(bytes, loc),
+            TokenKind::Configuration => repr.recycle_multiple(bytes, hint, loc),
         }
 
         self
@@ -62,18 +76,26 @@ enum TokenRepr {
 }
 
 impl TokenRepr {
-    fn recycle_single(&mut self, bytes: &[u8], loc: Location) {
+    const UNFINISHED_CONFIG: &str =
+        "configurator did not contain enough information to build token";
+
+    pub(crate) fn add_lexeme(&mut self, lex: impl AsRef<[u8]>) {
         match self {
-            Self::Empty => panic!("configurator did not contain enough information to build token"),
-            Self::Configuration(token_configurator) => {
-                token_configurator.recycle_single(bytes, loc)
-            }
+            Self::Empty => panic!("{}", Self::UNFINISHED_CONFIG),
+            Self::Configuration(token_configurator) => todo!(),
+        }
+    }
+
+    fn recycle_single(&mut self, byte: u8, loc: Location) {
+        match self {
+            Self::Empty => panic!("{}", Self::UNFINISHED_CONFIG),
+            Self::Configuration(token_configurator) => token_configurator.recycle_single(byte, loc),
         }
     }
 
     fn recycle_compound(&mut self, bytes: &[u8], loc: Location) {
         match self {
-            Self::Empty => panic!("configurator did not contain enough information to build token"),
+            Self::Empty => panic!("{}", Self::UNFINISHED_CONFIG),
             Self::Configuration(token_configurator) => {
                 token_configurator.recycle_compound(bytes, loc)
             }
@@ -82,7 +104,7 @@ impl TokenRepr {
 
     fn recycle_multiple(&mut self, bytes: &[u8], hint: TokenType, loc: Location) {
         match self {
-            Self::Empty => panic!("configurator did not contain enough information to build token"),
+            Self::Empty => panic!("{}", Self::UNFINISHED_CONFIG),
             Self::Configuration(token_configurator) => {
                 token_configurator.recycle_multiple(bytes, hint, loc)
             }
@@ -92,7 +114,7 @@ impl TokenRepr {
     fn finalize(&mut self) -> Token {
         match self {
             Self::Empty => {
-                panic!("configurator did not contain enough information to build a token")
+                panic!("{}", Self::UNFINISHED_CONFIG)
             }
             Self::Configuration(token_configurator) => token_configurator.finalize(),
         }
@@ -129,6 +151,7 @@ impl TokenRepr {
 struct TokenConfigurator {
     token_type: TokenType,
     loc: Location,
+    rest: Option<Delayed>,
 }
 
 impl TokenConfigurator {
@@ -136,6 +159,7 @@ impl TokenConfigurator {
         let Self {
             token_type,
             loc: self_loc,
+            ..
         } = self;
 
         *token_type = TokenType::single(byte);
@@ -146,10 +170,15 @@ impl TokenConfigurator {
         let Self {
             token_type,
             loc: self_loc,
+            ..
         } = self;
 
-        *token_type = TokenType::compound(byte);
+        *token_type = TokenType::compound(bytes);
         *self_loc = loc;
+    }
+
+    fn recycle_multiple(&mut self, bytes: &[u8], token: TokenType, loc: Location) {
+        todo!()
     }
 
     fn finalize(&mut self) -> Token {
@@ -164,6 +193,7 @@ impl TokenConfigurator {
         Self {
             token_type: TokenType::compound(bytes),
             loc,
+            rest: Option::default(),
         }
     }
 
@@ -171,6 +201,13 @@ impl TokenConfigurator {
         Self {
             token_type: TokenType::single(byte),
             loc,
+            rest: Option::default(),
         }
     }
+}
+
+#[derive(Debug)]
+struct Delayed {
+    lexeme: Lexeme,
+    lit: Option<Lit>,
 }
